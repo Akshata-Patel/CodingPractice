6f01aba41f6595db5bcedfa4ae9a70d92f9915e5
Akshata-Patel <patel.akshata29@gmail.com>
2021-02-24 18:09:10 +0530

    StarTask1 Successfully Completed.

---
+++ CodingPractice/ForLoop/StarTask1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodingPractice.ForLoop
+{
+    public class StarTask1
+    {
+        public void Execute()
+        {
+            int num;
+            Console.WriteLine("Please enter your number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int row = 1; row <= num; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodingPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using CodingPractice.ForLoop;
+
+namespace CodingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StarTask1 starTask1 = new StarTask1();
+            starTask1.Execute();
+        }
+    }
+}
